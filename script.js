8ed91a989697f262cd634dc453580e8c94a11a70
Vedant Kunte <vedantkunte8@gmail.com>
2021-04-24 17:24:04 +0530

    loader

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+const overlayText = document.getElementById("overlay-text");
+
+window.addEventListener("load", (event) => {
+  setTimeout(addoverlayText, 700);
+});
+
+function addoverlayText() {
+  overlayText.classList.add("overlayText-show");
+}
